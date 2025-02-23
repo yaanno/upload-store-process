b8dbf83bf4d0aaf5cbdf7e252bfaa2357e89e255
Janos Hardi <janos-karoly.hardi@telekom.com>
2025-02-23 11:04:34 +0100

    consolidate proto validation

--- gen/go/filestorage/v1/validation.go
+++ gen/go/filestorage/v1/validation.go
@@ -8,6 +8,43 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+func (req *DeleteFileRequest) Validate() error { // Method on the generated struct!
+	if req == nil {
+		return status.Errorf(codes.InvalidArgument, "request cannot be nil")
+	}
+	if req.UserId == "" {
+		return status.Errorf(codes.InvalidArgument, "user ID is required")
+	}
+	if req.FileId == "" {
+		return status.Errorf(codes.InvalidArgument, "file ID cannot be empty")
+	}
+	return nil
+}
+
+func (req *GetFileMetadataRequest) Validate() error { // Method on the generated struct!
+	if req == nil {
+		return status.Errorf(codes.InvalidArgument, "request cannot be nil")
+	}
+	if req.UserId == "" {
+		return status.Errorf(codes.InvalidArgument, "user ID is required")
+	}
+	if req.FileId == "" {
+		return status.Errorf(codes.InvalidArgument, "file ID cannot be empty")
+	}
+	return nil
+}
+
+func (req *ListFilesRequest) Validate() error { // Method on the generated struct!
+	if req == nil {
+		return status.Errorf(codes.InvalidArgument, "request cannot be nil")
+	}
+	if req.UserId == "" {
+		return status.Errorf(codes.InvalidArgument, "user ID is required")
+	}
+
+	return nil
+}
+
 func (req *PrepareUploadRequest) Validate() error { // Method on the generated struct!
 	if req == nil {
 		return status.Errorf(codes.InvalidArgument, "request cannot be nil")
